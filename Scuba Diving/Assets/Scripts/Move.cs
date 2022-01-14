b3bf81e2f2bb8308676ae25bc5b6945af243e931
rudolf0328 <ck0716080@gmail.com>
2022-01-14 15:01:21 +0900

    please

--- Scuba Diving/Assets/Scripts/Move.cs
+++ Scuba Diving/Assets/Scripts/Move.cs
@@ -8,7 +8,13 @@ public class Move : MonoBehaviour
     Vector2 m_moveLimit = new Vector2(4.0f, 0);
 
     Vector3 right = new Vector3(1, 0, 0);
-    Vector3 forward = new Vector3(0, 1, 0);
+    Vector3 forward = new Vector3(0, 0, 1);
+    Vector3 upward = new Vector3(0, 1, 0);
+
+    float horizontalSpeed = 2.0f;
+    float verticalSpeed = 2.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +26,21 @@ public class Move : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        // float t = Input.GetAxis("")
 
         h = h * Time.deltaTime;
         v = v * Time.deltaTime;
 
         transform.Translate(right * h * 20);
         transform.Translate(forward * v * 20);
+
+        if(Input.GetMouseButton(0)) {
+          // Get the mouse delta. This is not in the range -1...1
+          float h1 = horizontalSpeed * Input.GetAxis("Mouse X");
+          float v1 = verticalSpeed * Input.GetAxis("Mouse Y");
+
+          transform.Rotate(v1, h1, 0);
+        }
     }
 
   public Vector3 ClampPosition(Vector3 position) {
